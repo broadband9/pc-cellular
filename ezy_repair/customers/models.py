9c1b678fa6ad9cfae808529cb04fde0e28765c9d
MugheesMehdi07 <mugheesmehdi@gmail.com>
2025-01-29 22:10:19 +0500

    set defauklt in customer

--- ezy_repair/customers/models.py
+++ ezy_repair/customers/models.py
@@ -3,8 +3,8 @@ from django.db import models
 class Customer(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    postcode = models.CharField(max_length=100, null=True, blank=True)
-    email = models.EmailField(max_length=200, null=True, blank=True)
+    postcode = models.CharField(max_length=100, null=True, blank=True, default="")
+    email = models.EmailField(max_length=200, null=True, blank=True, default="")
     phone = models.CharField(max_length=15, unique=True)
 
     def __str__(self):
